943d83c4703d33c3dc80a4029ebfbd56f944c7f6
Tomas Lin <horsekit1982@gmail.com>
2018-02-14 15:59:58 +0800

    增加影片上傳 Loading

--- src/components/CreateStore/ImageHandler.js
+++ src/components/CreateStore/ImageHandler.js
@@ -3,19 +3,32 @@ import propTypes from "prop-types";
 import Dropzone from "react-dropzone";
 import Image from "react-image";
 import styled from "styled-components";
+import ReactLoading from "react-loading";
+import { white, grey500 } from "material-ui/styles/colors";
 
 const ImageItem = styled(Image)`
   width: 100%;
   height: 100%;
 `;
 
+const StyleDropzone = styled(Dropzone)`
+  border: 0px solid ${white};
+  width: 25vw;
+  height: 40vh;
+`;
+
 const ImageHandler = props => {
+  let content = <ImageItem src={props.image} />;
+
+  if (props.isLoading) {
+    content = (
+      <ReactLoading type="spin" color={grey500} height="100%" width="100%" />
+    );
+  }
   return (
-    <Dropzone onDrop={files => props.uploadImage(files[0])}>
-      <ImageItem
-        src={props.image}
-      />
-    </Dropzone>
+    <StyleDropzone onDrop={files => props.uploadImage(files[0])}>
+      {content}
+    </StyleDropzone>
   );
 };
 
@@ -27,7 +40,8 @@ ImageHandler.propTypes = {
 };
 
 ImageHandler.defaultProps = {
-  image: "https://ostarmotorsports.com/images/Unavailable/256px-No_image_available.svg.png"
+  image:
+    "https://ostarmotorsports.com/images/Unavailable/256px-No_image_available.svg.png"
 };
 
 export default ImageHandler;
